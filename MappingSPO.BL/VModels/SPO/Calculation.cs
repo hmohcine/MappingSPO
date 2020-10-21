7366825b9f00877316b774b3e75f5c5b94a17b33
hmohcine <61138229+hmohcine@users.noreply.github.com>
2020-10-21 09:02:14 +0100

    - commit

---
+++ MappingSPO.BL/VModels/SPO/Calculation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MappingSPO.Framework.BL.VModels.SPO
+{
+    public class Calculation
+    {
+        public string Status { get; set; }
+        public int Nummer { get; set; }
+        public string Versie { get; set; }
+        public string Naam { get; set; }
+        public string Calculator { get; set; }
+        public int CalculatorId { get; set; }
+        public string Bedrijf { get; set; }
+    }
+}
